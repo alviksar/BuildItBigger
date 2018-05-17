597c93aac20052d0b028a00ce82cda820a940724
Alexey Sarafannikov <alviksar@gmail.com>
2018-05-17 23:04:20 +0300

    Add a JokerTest class

---
+++ jokeLibrary/src/test/java/xyz/alviksar/jokelibrary/JokerTest.java
@@ -0,0 +1,16 @@
+package xyz.alviksar.jokelibrary;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JokerTest {
+
+    /**
+     * Checks that a Joker class returns not empty string.
+     */
+    @Test
+    public void getJoke() {
+        assert !Joker.getJoke().isEmpty();
+    }
+}
