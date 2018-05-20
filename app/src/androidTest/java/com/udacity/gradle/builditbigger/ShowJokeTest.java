fb19ee9b7ea8b05c01a99afca6f2d3670cbbda9d
Alexey Sarafannikov <alviksar@gmail.com>
2018-05-20 14:51:50 +0300

    Repare runAndroidTests

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/ShowJokeTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ShowJokeTest.java
@@ -1,12 +1,23 @@
 package com.udacity.gradle.builditbigger;
 
+import android.support.test.espresso.IdlingRegistry;
+import android.support.test.espresso.IdlingResource;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.IsNot.not;
 
 /**
  * Instrumented test, which will check that joke is displayed on an Android device.
